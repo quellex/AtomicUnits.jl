9eba1959adbcb647983c8d0af2194567e38086cd
Takuma Teramura <=>
2021-07-01 12:26:07 +0900

    fix vnm2i

--- src/AtomicUnits.jl
+++ src/AtomicUnits.jl
@@ -10,22 +10,25 @@ const fac_vnm2au = 514.2	# amplitude [V/nm] → amplitude [a.u.]
 const fac_au2as  = 24.1899	# time [a.u.] → time[atto sec.]
 const fac_wleneV = 1239.84190   # wlen [n.m.] * energy [eV]
 const fac_au2eV = 27.2113845	# energy [a.u.] → energy [eV]
-# convert functions
+## convert functions
+# laser amplitude
 i2e(fint) = sqrt(fint / fac_i2e)
 e2i(famp) = famp^2 * fac_i2e
-vnm2e(famp_vnm) = fac_vnm2au / famp_vnm
+vnm2e(famp_vnm) = fac_vnm2au / famp_vnm  
 e2vnm(famp_au) = famp_au * fac_vnm2au
 vnm2i(famp_vnm) = e2i(vnm2e(famp_vnm))
 i2vnm(fint) = e2vnm(i2e(fint))
+A2i(A, wlen) = e2i(A*wlen2au(wlen))
+i2A(fint, wlen) = i2e(fint)/wlen2au(wlen)
+## time
 au2as(t_au) = fac_au2as * t_au
 as2au(t_as) = t_as / fac_au2as
+## wavelength and photon energy
 wlen2au(wlen_nm) = wlen2eV(wlen_nm) / fac_au2eV
 au2wlen(freq_au) = fac_au2eV * freq_au
 wlen2eV(wlen_nm) = fac_wleneV / wlen_nm
 eV2wlen(ene_eV) = fac_wleneV / ene_eV
 au2eV(ene_au) = fac_au2eV*ene_au
 eV2au(ene_eV) = fac_au2eV/ene_eV
-A2i(A, wlen) = e2i(A*wlen2au(wlen))
-i2A(fint, wlen) = i2e(fint)/wlen2au(wlen)
 end
-#
+
