ee7b7ab98280eff7a50123446e797cfdab665558
Takuma Teramura <=>
2021-07-01 12:17:20 +0900

    vnm2i

--- src/AtomicUnits.jl
+++ src/AtomicUnits.jl
@@ -2,7 +2,8 @@
 # unit conversion
 module AtomicUnits
 export fac_i2e, fac_vnm2au, fac_wlenev, fac_au2eV
-export i2e, e2i, vnm2au, au2vnm, au2as, as2au, wlen2au, au2wlen, wlen2eV, eV2wlen, eV2au, au2eV
+export i2e, e2i, vnm2e, e2vnm, i2vnm, vnm2i
+export au2as, as2au, wlen2au, au2wlen, wlen2eV, eV2wlen, eV2au, au2eV
 export A2i, i2A
 const fac_i2e  = 3.50944506e16   # intensity [W/cm^2] → amplitude [a.u.]
 const fac_vnm2au = 514.2	# amplitude [V/nm] → amplitude [a.u.]
@@ -12,8 +13,10 @@ const fac_au2eV = 27.2113845	# energy [a.u.] → energy [eV]
 # convert functions
 i2e(fint) = sqrt(fint / fac_i2e)
 e2i(famp) = famp^2 * fac_i2e
-vnm2au(famp_vnm) = fac_vnm2au * famp_vnm
-au2vnm(famp_au) = famp_au / fac_vnm2au
+vnm2e(famp_vnm) = fac_vnm2au / famp_vnm
+e2vnm(famp_au) = famp_au * fac_vnm2au
+vnm2i(famp_vnm) = e2i(vnm2e(famp_vnm))
+i2vnm(fint) = e2vnm(i2e(fint))
 au2as(t_au) = fac_au2as * t_au
 as2au(t_as) = t_as / fac_au2as
 wlen2au(wlen_nm) = wlen2eV(wlen_nm) / fac_au2eV
